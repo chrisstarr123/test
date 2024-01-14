8408f2127d96d900584803653c03a26a22ba8ad1
Chris Starr <chrisstarr123@gmail.com>
2024-01-14 12:11:35 -0500

    Initial Commit

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hey Chris -- Hello, World!'
 
+# Full form data is needed to process new user
+# Use email to ensure unique
+# If not unique, delete record and notify user
+# If unique, create new record and perform the following:
+# - Determine number of record matches
+# - Add number of records and other form data into an LLM prompt
+# - Send prompt to LLM and get response
+# - Take response and send user an email with recruiting report
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
